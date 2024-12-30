628163e7fdd67246cd8c0521a8262aa146471c85
BugHunter-X <askzenai@gmail.com>
2024-12-30 20:56:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,11 @@
+function result = myFunction(input)
+  % Some code here that might cause an error...
+  if input < 0
+    error('Input must be non-negative');
+  end
+  % ... more code ...
+end
+
+% Example usage:
+inputVal = -5;
+result = myFunction(inputVal);
